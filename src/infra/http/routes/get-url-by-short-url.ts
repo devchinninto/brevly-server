954b0cf6f8eeae344a3dc413c912902ecc34ba87
marcelle alves <marcellealves.dev@gmail.com>
2026-09-04 18:37:09 -0300

    feat: implement route to retrieve original URL by short URL

---
+++ src/infra/http/routes/get-url-by-short-url.ts
@@ -0,0 +1,48 @@
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import z from 'zod'
+import { getUrl } from '@/app/use-cases/get-url.ts'
+import { isRight, unwrapEither } from '@/shared/either.ts'
+
+export const getUrlByShortUrlRoute: FastifyPluginAsyncZod = async (server) => {
+  server.get(
+    '/urls/:shortUrl',
+    {
+      schema: {
+        summary: 'Get an original URL by short URL',
+        tags: ['List'],
+        params: z.object({
+          shortUrl: z.string()
+        }),
+        response: {
+          200: z
+            .object({
+              originalUrl: z.string()
+            })
+            .describe('Get an original URL by short URL.'),
+
+          404: z
+            .object({
+              message: z.string()
+            })
+            .meta({ example: { message: 'Url not found.' } })
+            .describe('Url not found.')
+        }
+      }
+    },
+    async (request, reply) => {
+      const { shortUrl } = request.params
+
+      const result = await getUrl(shortUrl)
+
+      if (isRight(result)) {
+        const originalUrl = unwrapEither(result)
+
+        return reply.status(200).send({ originalUrl })
+      }
+
+      const error = unwrapEither(result)
+
+      return reply.status(404).send({ message: error.message })
+    }
+  )
+}

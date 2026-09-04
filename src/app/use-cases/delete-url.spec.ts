c2eeb8c18eedaefb9b6f7c5fdf7f3e888b291a56
marcelle alves <marcellealves.dev@gmail.com>
2026-09-04 17:55:29 -0300

    feat: add `UrlNotFoundError` class and update `deleteUrl` function

--- src/app/use-cases/delete-url.spec.ts
+++ src/app/use-cases/delete-url.spec.ts
@@ -2,10 +2,10 @@ import { describe, expect, it, beforeEach, afterEach } from 'vitest'
 import { createShortUrl } from './create-short-url.ts'
 import { db } from '@/infra/db/index.ts'
 import { schema } from '@/infra/db/schemas/index.ts'
-import { listUrls } from './list-urls.ts'
 import { uuidv7 } from 'uuidv7'
 import { deleteUrl } from './delete-url.ts'
 import { InvalidUrlFormatError } from '../errors/invalid-url-format.ts'
+import { listUrls } from './list-urls.ts'
 
 beforeEach(async () => {
   await db.delete(schema.urls)
@@ -32,10 +32,17 @@ describe('Delete a short url', () => {
       throw new InvalidUrlFormatError()
     }
 
-    await deleteUrl(url)
+    const _deleted = await deleteUrl(url)
+
+    if (!_deleted) {
+      throw new InvalidUrlFormatError()
+    }
+
+    const deleted = _deleted.right
 
     const result = await listUrls()
 
     expect(result.urls.length).toBe(0)
+    expect(deleted?.shortUrl).toEqual(createdUrl.right?.shortUrl)
   })
 })
--- src/app/use-cases/delete-url.ts
+++ src/app/use-cases/delete-url.ts
@@ -3,13 +3,21 @@ import { schema } from '@/infra/db/schemas/index.ts'
 import { eq } from 'drizzle-orm'
 import { z } from 'zod'
 import { InvalidUrlFormatError } from '../errors/invalid-url-format.ts'
-import { makeRight } from '@/shared/either.ts'
+import { UrlNotFoundError } from '../errors/url-not-found-error.ts'
+import { Either, makeLeft, makeRight } from '@/shared/either.ts'
 
 const deleteUrlInput = z.string()
 
 type DeleteUrlInput = z.input<typeof deleteUrlInput>
 
-export async function deleteUrl(url: DeleteUrlInput) {
+type DeleteUrlOutput = typeof schema.urls.$inferSelect
+
+type deleteUrlOutput = Either<
+  InvalidUrlFormatError | UrlNotFoundError,
+  DeleteUrlOutput
+>
+
+export async function deleteUrl(url: DeleteUrlInput): Promise<deleteUrlOutput> {
   const parsed = deleteUrlInput.safeParse(url)
 
   if (!parsed.success) {
@@ -19,6 +27,11 @@ export async function deleteUrl(url: DeleteUrlInput) {
   const deleted = await db
     .delete(schema.urls)
     .where(eq(schema.urls.shortUrl, parsed.data))
+    .returning()
 
-  return makeRight({ deleted })
+  if (deleted.length === 0) {
+    return makeLeft(new UrlNotFoundError())
+  }
+
+  return makeRight(deleted[0])
 }

02d5d5a0c894b933bdad11f8de22561127307102
marcelle alves <marcellealves.dev@gmail.com>
2026-09-04 14:15:29 -0300

    test: enhance tests for `createShortUrl` to include error handling for invalid URLs and duplicates

--- src/app/use-cases/create-short-url.spec.ts
+++ src/app/use-cases/create-short-url.spec.ts
@@ -1,29 +1,61 @@
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it, beforeAll } from 'vitest'
 import { createShortUrl } from './create-short-url.ts'
+import { db } from '@/infra/db/index.ts'
+import { schema } from '@/infra/db/schemas/index.ts'
+import { isRight, unwrapEither } from '@/shared/either.ts'
+import { env } from '@/env.ts'
+import { InvalidUrlFormatError } from '../errors/invalid-url-format.ts'
+import { UrlAlreadyExistsError } from '../errors/url-already-exists-error.ts'
+
+beforeAll(async () => {
+  await db.delete(schema.urls)
+})
 
 describe('create a short url', () => {
-  it('should be able to create a new short url', async () => {
+  it('should create a new short url', async () => {
     const input = {
-      name: 'google',
       originalUrl: 'https://google.com',
       shortUrlHandle: 'google'
     }
 
-    const result = await createShortUrl(input)
-    console.log(result)
+    const createdUrl = await createShortUrl(input)
+    const formattedShortUrl = `${env.PREFIX}/${input.shortUrlHandle}`
 
-    expect(result?.shortUrl).toBe('brevly.com/google')
+    let result
+
+    if (isRight(createdUrl)) {
+      result = unwrapEither(createdUrl)
+    }
+
+    expect(result?.originalUrl).toEqual(input.originalUrl)
+    expect(result?.shortUrl).toEqual(formattedShortUrl)
   })
 
-  it('should NOT be able to create a new short url', async () => {
+  it('should throw an Invalid URL Format Error', async () => {
     const input = {
-      name: 'google',
-      originalUrl: 'https://mywebsite.com/api/something',
-      shortUrlHandle: 'mywebsite/api/something'
+      originalUrl: 'https://mywebsite.com/',
+      shortUrlHandle: 'mywebsite_new_big_link'
     }
 
     const result = await createShortUrl(input)
 
-    expect(result?.shortUrl).toThrow(TypeError)
+    const error = unwrapEither(result)
+
+    expect(error).toBeInstanceOf(InvalidUrlFormatError)
+  })
+
+  it('should throw an URL Already Exists Error', async () => {
+    const input = {
+      originalUrl: 'https://mywebsite.com/',
+      shortUrlHandle: 'mywebsite'
+    }
+
+    const firstCreateAttempt = await createShortUrl(input)
+
+    const secondCreateAttempt = await createShortUrl(input)
+    const error = unwrapEither(secondCreateAttempt)
+
+    expect(isRight(firstCreateAttempt)).toBe(true)
+    expect(error).toBeInstanceOf(UrlAlreadyExistsError)
   })
 })
